8116894ba6267beb0bb2137add9a2085d3e9784e
norhan-sa <norhan.sa200@gmail.com>
2020-12-02 13:10:35 +0200

    new version

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
  const  social   =  require('./routes/social');
  const    app    =  express();
  
+ app.use(express.json());
  app.use('/',social);
  app.get('/',(req,res)=>{
    return res.send('We Are Connected');
