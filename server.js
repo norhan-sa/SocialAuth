a8c5d9fffaec00f339868bd4577994113c4a1960
norhan-sa <norhan.sa200@gmail.com>
2020-12-08 20:55:37 +0200

    first commit

--- server.js
+++ server.js
@@ -3,16 +3,16 @@
  const   app        =    require('./app');
  const   http       =    require('http').createServer(app);
  const    io        =    require('socket.io')(http , { origins: '*:*'}); 
- const socketioJwt  =    require('socketio-jwt');
  const    db        =    require('./config/db');
  let   main_chat    =    require('./chat');
  let     port       =    process.env.PORT || 5000; 
  let   jwt_key      =    process.env.JWT_SECRET; 
  
- io.use(socketioJwt.authorize({
-   secret: jwt_key,
-   handshake: true
- }));
+//  io.use(socketioJwt.authorize({
+//    secret: jwt_key,
+//    handshake: true,
+//    auth_header_required: true
+//  }));
 
  let chat_nsp = io.of('messages');
  main_chat(chat_nsp);
