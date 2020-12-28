3468f2b6ec52f7b629b67255619eabf276cebb7d
norhan-sa <norhan.sa200@gmail.com>
2020-12-28 20:27:15 +0200

    first_commit

--- chat.js
+++ chat.js
@@ -1,45 +1,59 @@
+ require('dotenv').config();
  const   Users       =    require('./config/db').Users;
+ const   JWT_SEC     =    process.env.JWT_SECRET;
+ const    jwt        =    require('jsonwebtoken');
  let  active_users   =    [];
 
  function main_chat(nsp){
-  
-     nsp.on('connection',function(socket){
+
+   nsp.use(function(socket, next){
+      if (socket.handshake.query && socket.handshake.query.token){
+        jwt.verify(socket.handshake.query.token, JWT_SEC , function(err, decoded) {
+          if (err) return next(new Error('Authentication error'));
+          socket.decoded = decoded;
+          next();
+        });
+      }
+      else {
+        next(new Error('Authentication error'));
+      }    
+    }).on('connection',function(socket){
          console.log(`user connected ${socket.id} `);
  
-         //  J O I N   T H E   C H A T (get user data from the db)
-         socket.on('join chat', function(data){
-            let is_exist = getSocketId(data.id);
-            if(is_exist){
-               active_users[is_exist.index].socket.push(socket.id);
-               socket.emit('join chat',{msg: 'join the chat with another device', data: active_users[is_exist.index], status: 200});
-            }else 
-            Users.findOne({where: {id: data.id}}).then((user)=>{
-             if(user){  
-               let data = {
-                  id: user.id, 
-                  firstname: user.firstname,
-                  lastname: user.lastname, 
-                  socket: [socket.id],
-                  picture: user.picture,
-                  active: true
-               }
-               active_users.push(data);
-               socket.emit('join chat',{msg:'you join the chat successfully', data: data, status:200});
-               socket.broadcast.emit(`active users`,data);
-             }else{
-               socket.emit('join chat',{msg:'you are not registered', data: null, status:400});  
-             } 
-            });
-         });
+         // //  J O I N   T H E   C H A T (get user data from the db)
+         // socket.on('join chat', function(data){
+         //    let is_exist = getSocketId(data.id);
+         //    if(is_exist){
+         //       active_users[is_exist.index].socket.push(socket.id);
+         //       socket.emit('join chat',{msg: 'join the chat with another device', data: active_users[is_exist.index], status: 200});
+         //    }else 
+         //    Users.findOne({where: {id: data.id}}).then((user)=>{
+         //     if(user){  
+         //       let data = {
+         //          id: user.id, 
+         //          firstname: user.firstname,
+         //          lastname: user.lastname, 
+         //          socket: [socket.id],
+         //          picture: user.picture,
+         //          active: true
+         //       }
+         //       active_users.push(data);
+         //       socket.emit('join chat',{msg:'you join the chat successfully', data: data, status:200});
+         //       socket.broadcast.emit(`active users`,data);
+         //     }else{
+         //       socket.emit('join chat',{msg:'you are not registered', data: null, status:400});  
+         //     } 
+         //    });
+         // });
 
-         //  G E T   A C T I V E   U S E R S 
-         socket.on(`active users`,function(data){
-            socket.emit(`active users`, {
-                msg:'successfully get active users', 
-                data: getActiveUsers(data.id), 
-                status: 200
-            });
-         });
+         // //  G E T   A C T I V E   U S E R S 
+         // socket.on(`active users`,function(data){
+         //    socket.emit(`active users`, {
+         //        msg:'successfully get active users', 
+         //        data: getActiveUsers(data.id), 
+         //        status: 200
+         //    });
+         // });
          
          //  D I S C O N N E C T   F R O M   T H E   C H A T
          socket.on('disconnect',function(){
