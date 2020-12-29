0862dc1c6e82b1b6d79afcbb1aa1f3997e17113f
norhan-sa <norhan.sa200@gmail.com>
2020-12-30 00:20:11 +0200

    save changes

--- chat.js
+++ chat.js
@@ -41,7 +41,7 @@
 
     //  G E T   A L L   A C T I V E   U S E R S 
     socket.emit('active users', activeUsers(nsp, socket.id));
-    console.log('rrrrrrrr');
+
 
     //  N O T I F Y   U S E R S   F O R   N E W   C O N N E C T I O N
     socket.broadcast.emit('active users', socket.data);
@@ -54,9 +54,9 @@
         let to_user_data = findById(nsp , toID);
         if(to_user_data){
         let to_socket_id = to_user_data.id;
-        socket.to(to_socket_id).emit('private message',{msg: msg, from: from, with: from});
+        socket.to(to_socket_id).emit('private message',{msg: msg, from: from, with: from, date: new Date()});
         }
-        socket.emit('private message',{msg: msg, from: from, with: toID});
+        socket.emit('private message',{msg: msg, from: from, with: toID, date: new Date()});
     });
 
     //  D I S C O N N E C T   F R O M   T H E   C H A T
@@ -69,37 +69,42 @@
 
  // F I N D  A L L  A C T I V E  U S E R S
  function activeUsers(nsp , id){
-
-    var clients = nsp.clients().connected;
-    var array = Object.keys(clients)
-    .map(function(key) {
-        return clients[key];
-    });
+      
+    let active_sockets = getConnectedSocketsArray(nsp);
 
     let activeusers = [];
-
-    for(let i = 0 ; i < array.length ; ++i){
-        if(array[i].id == id)
+    for(let i = 0 ; i < active_sockets.length ; ++i){
+        if(active_sockets[i].id == id)
         continue;
-        else activeusers.push(array[i].data);
+        else activeusers.push(active_sockets[i].data);
     }
     console.log(activeusers);
     return activeusers;
  }
 
+ // F I N D  A C T I V E   U S E R   S O C K E T   I D   (BY ID)
  function findById(nsp ,id){
 
+   let active_sockets = getConnectedSocketsArray(nsp);
+   
+   for(let i = 0 ; i < active_sockets.length ; ++i){
+      if(active_sockets[i].data.id === id) 
+         return active_sockets[i];
+   }
+     return;
+ }
+
+
+ //  G E T   C O N N E C T E D   S O C K E T S  A S   A R R A Y
+ function getConnectedSocketsArray(nsp){
     var clients = nsp.clients().connected;
     var array = Object.keys(clients)
     .map(function(key) {
         return clients[key];
     });
-   
-   for(let i = 0 ; i < array.length ; ++i){
-       if(array[i].data.id === id) 
-         return array[i];
-   }
-     return;
+
+    return array;
  }
+
 
  exports.main_chat = main_chat;
